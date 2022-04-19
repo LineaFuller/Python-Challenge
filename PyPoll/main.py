48c55d913927f7b248186362f26f9be2b1c686c5
LineaFuller <lineafuller2@gmail.com>
2022-04-19 13:16:25 -0700

    updates

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,53 +1,61 @@
-
 import os
 import csv
 
 # Path to collect data from resources folder
 election_data_csv = os.path.join("Resources","election_data.csv")
 
-# Declare Variables
-count = 0
-candidatelist = []
-candidate_options = []
-vote_count = []
-vote_percent = []
-
 
 # Read in the csv file 
 with open(election_data_csv, 'r') as csv_file: 
     csv_reader = csv.reader(csv_file, delimiter=',')
-    csv_header = next(csv_reader)
+
+    # Declare Variables 
+    header = next(csv_reader)
+    count = 0
+    candidate_list = []
+    candidate_options = []
+    vote_count = []
+    vote_percent = []
+
     # Loop through data with algorithmic calculations to produce results
     for row in csv_reader:
         # Count the total number of votes
         count = count + 1
-        # Set the candidate names to candidatelist
-        candidatelist.append(row[2])
+        # Add it to the list of candidates in the running
+        candidate_list.append(row[2])
         # Create a set from the candidatelist to get the unique candidate names
-    for x in set(candidatelist):
+    for x in set(candidate_list):
         candidate_options.append(x)
         # y is the total number of votes per candidate
-        y = candidatelist.count(x)
+        y = candidate_list.count(x)
         vote_count.append(y)
         # z is the percent of total votes per candidate
         z = (y/count)*100
         vote_percent.append(z)
-        
+    
+    
+   # Then add a vote to that candidate's count
     winning_vote_count = max(vote_count)
     winner = candidate_options[vote_count.index(winning_vote_count)]
-    
 
-# Print the results and export the data to our text file
-with open("Analysis.txt", 'w') as text:
-    text.write("Election Results\n")
-    text.write("---------------------------------------\n")
-    text.write("Total Vote: " + str(count) + "\n")
-    text.write("---------------------------------------\n")
+
+# For each Candidate...
     for i in range(len(set(candidate_options))):
-        text.write(candidate_options[i] + ": " + str(vote_percent[i]) +"% (" + str(vote_count[i]) + ")\n")
-    text.write("---------------------------------------\n")
-    text.write("The winner is: " + winner + "\n")
-    text.write("---------------------------------------\n")
+        # Print Name, Percent, and Total Vote Count
+            print(candidate_options[i] + str(vote_percent[i]) + "%" + str(vote_count[i]))
 
 
-    
+# Export the results to a text file under Analysis
+
+pypoll_file = os.path.join("Analysis", "analysis.txt")
+with open(pypoll_file, "w") as outfile:
+
+    outfile.write("Election Results\n")
+    outfile.write("---------------------------\n")
+    outfile.write(f"Total Vote: ${str(count)}\n")
+    outfile.write("-------------------------\n")
+    for i in range(len(set(candidate_options))):
+            outfile.write(f"{candidate_options[i]}: {(round(vote_percent[i]))}%, {str(vote_count[i])} Votes\n")
+    outfile.write("---------------------------------------\n")
+    outfile.write(f"The winner is: {winner}\n")
+    outfile.write("---------------------------------------\n")
